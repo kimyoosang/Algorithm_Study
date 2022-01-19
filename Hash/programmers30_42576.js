63d3ac52f41c2226b83f2d437836e5646d5d8cf7
kimyoosang <yoo99481@wemember.kr>
2022-01-19 15:02:11 +0900

    [Update] programmers_42576 another solution

--- Hash/programmers30_42576.js
+++ Hash/programmers30_42576.js
@@ -1,6 +1,7 @@
 //https://programmers.co.kr/learn/courses/30/lessons/42576
 //완주하지 못한 선수
 
+//1
 function solution(participant, completion) {
   participant.sort()
   completion.sort()
@@ -10,4 +11,19 @@ function solution(participant, completion) {
       return participant[i] //여기서 바로 return
     }
   }
+}
+
+//2. 1번과 별차이 없어보이는데 효율성 테스트에서 아주 약간 더 느렸음
+function solution(participant, completion) {
+  var answer = ''
+  participant.sort()
+  completion.sort()
+
+  for (let i = 0; i < participant.length; i++) {
+    if (participant[i] !== completion[i]) {
+      answer = participant[i]
+      break
+    }
+  }
+  return answer
 }
